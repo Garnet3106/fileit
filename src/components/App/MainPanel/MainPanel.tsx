cdd2880f5b88d2963e047561409aef4a32b532d1
Garnet3106 <manage@gant.work>
2022-10-13 17:47:13 +0900

    Add operation icons in the tab list

--- src/components/App/MainPanel/MainPanel.tsx
+++ src/components/App/MainPanel/MainPanel.tsx
@@ -7,13 +7,17 @@ import TabBar from './TabBar/TabBar';
 import './MainPanel.css';
 import { FileItemIdentifier, Item } from '../../../item';
 
+export const variables = {
+    width: `calc(100vw - ${leftPanelVariables.width + 1}px)`,
+};
+
 export default function MainPanel() {
     const containerBorderWidth = 1;
 
     const styles = {
         container: {
             borderLeft: `${containerBorderWidth}px solid ${preferences.appearance.border2}`,
-            width: `calc(100% - ${leftPanelVariables.width + containerBorderWidth}px)`,
+            width: variables.width,
         },
     };
 
--- src/components/App/MainPanel/TabBar/TabBar.tsx
+++ src/components/App/MainPanel/TabBar/TabBar.tsx
@@ -12,12 +12,25 @@ export default function TabBar() {
             backgroundColor: preferences.appearance.background.panel2,
             height: `${variables.height}px`,
         },
+        items: {
+            width: `calc(100vw - ${150 + 1}px)`,
+        },
     };
 
     return (
         <div className="tab-bar-container" style={styles.container}>
-            <TabItem selected={true} title="Folder1" icon={TabItemIcon.Folder} />
-            <TabItem selected={false} title="Folder2" icon={TabItemIcon.CompressedFolder} />
+            <div className="tab-bar-items" style={styles.items}>
+                <TabItem selected={true} title="Folder1" icon={TabItemIcon.Folder} />
+                <TabItem selected={false} title="Folder2" icon={TabItemIcon.CompressedFolder} />
+            </div>
+            <div className="tab-bar-operations">
+                <div className="tab-bar-operation-icon" style={{
+                    backgroundImage: `url(../../../../../../lib/img/icons/dark/window/minimize.svg)`,
+                }} />
+                <div className="tab-bar-operation-icon" style={{
+                    backgroundImage: `url(../../../../../../lib/img/icons/dark/window/close.svg)`,
+                }} />
+            </div>
         </div>
     );
 }
