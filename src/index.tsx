9be80a5eebfe7b8fa5492e9e04f75b04c1554e1b
Garnet3106 <manage@gant.work>
2022-10-15 20:38:56 +0900

    Fix indent style of index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,19 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App/App';
 import reportWebVitals from './reportWebVitals';
-import Fs from './common/fs';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
-
-Fs.readFile('')
-  .then(console.log);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
